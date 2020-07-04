32df84303c8ca94ff7fa0f3a72f3d19c821a777a
Noel Zacharia <noelzacharia8@gmail.com>
2020-07-04 13:48:29 -0400

    Add all files

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a project that was made to be used as a Mosaic Painter, resembling the mosaic paintings of lore. It contains a resizable grid with several customizable options for styling each grid. It was made using JavaScript, CSS and HTML.
---
+++ script.js
@@ -0,0 +1,86 @@
+const grid = document.getElementById("grid");
+const gridSquare = document.createElement("div");
+const Buttonreset = document.querySelector(".buttonreset");
+const ButtonBlack = document.querySelector(".buttonblack");
+const colourButton = document.querySelector(".buttoncolour");
+const gridButton = document.querySelector(".buttongrid");
+let gridSquaresClass;
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+function clearGridSquares() {
+    while (grid.hasChildNodes()) {
+        grid.removeChild(grid.lastElementChild);
+    }
+}
+
+function drawGrid(numSquares = 18) {
+    for (let i = 0; i < numSquares ** 2; i++) {
+        const gridSquare = document.createElement("div");
+        gridSquare.classList.add("grid-item");
+        grid.appendChild(gridSquare);
+    }
+    grid.style.cssText = `grid-template-columns: repeat(${numSquares}, 1fr); grid-template-rows: repeat(${numSquares}, 1fr)`;
+}
+
+function addColourClass() {
+    gridSquaresClass = Object.values(document.getElementsByClassName("grid-item"));
+    gridSquaresClass.forEach(function(gridSquare) {
+        gridSquare.addEventListener('mouseover', () => {
+            gridSquare.classList.add("grid-item--colour");
+        });
+    });
+}
+
+function drawInitialGrid(numSquares) {
+    drawGrid(numSquares)
+    addColourClass();
+}
+drawInitialGrid(18);
+
+function resetBoard() {
+    gridSquaresClass.forEach(function(gridSquare) {
+        gridSquare.classList.remove("grid-item--colour");
+        gridSquare.style.removeProperty("background-color");
+    });
+}
+Buttonreset.addEventListener('click', resetBoard);
+
+function changeGrid() {
+    let numSquares = (prompt("Enter the number of squares"));    
+    clearGridSquares();
+    drawGrid(parseInt(numSquares));
+    addColourClass(); 
+}
+gridButton.addEventListener('click', changeGrid);
+
+function randomColour() {
+    return `rgb(${Math.round(Math.random() * 256)}, ${Math.round(Math.random() * 256)}, ${Math.round(Math.random() * 256)})`;
+}
+
+function randomRGB() {
+    gridSquaresClass = Object.values(document.getElementsByClassName("grid-item"));
+    gridSquaresClass.forEach(function(gridSquare) {
+        gridSquare.addEventListener('mouseover', () => {
+            gridSquare.style.cssText = `background-color: ${randomColour()}`;
+        });
+    });
+}
+colourButton.addEventListener('click', () => {
+    randomRGB();
+});
+
+function black() {
+    gridSquaresClass = Object.values(document.getElementsByClassName("grid-item"));
+    gridSquaresClass.forEach(function(gridSquare) {
+        gridSquare.addEventListener('mouseover', () => {
+            gridSquare.style.cssText = `background-color: black`;
+        });
+    });
+}
+ButtonBlack.addEventListener('click', () => {
+    black();
+});
